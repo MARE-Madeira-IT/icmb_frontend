b4446fec72ecb915fd17cc1546757825b57dccf8
Rúben Freitas <joseruben98@hotmail.com>
2025-01-10 15:11:42 +0000

    working chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -5,122 +5,120 @@ import MessageInput from "./Chat/MessageInput";
 import Echo from "laravel-echo";
 import { connect } from "react-redux";
 import {
-  createMessage,
-  fetchMessages,
+    createMessage,
+    fetchMessages,
 } from "../redux/redux-modules/message/actions";
 import { useParams } from "react-router-dom";
 import { fetchChat } from "../redux/redux-modules/chat/actions";
 
 function Chat(props) {
-  // const [currentEcho, setCurrentEcho] = useState(new Echo(options))
-  // const webSocketChannel = `chat`;
-  const webSocketChannel = `chats.${props.currentChat.socket}`;
-  const { messages, loading } = props;
-  const scroll = useRef();
-  const [allMessages, setAllMessages] = useState([]);
-  const [currentEcho, setCurrentEcho] = useState(undefined);
-  const { id } = useParams();
+    // const [currentEcho, setCurrentEcho] = useState(new Echo(options))
+    // const webSocketChannel = `chat`;
+    const webSocketChannel = `chats.${props.currentChat.socket}`;
+    const { messages, loading } = props;
+    const scroll = useRef();
+    const [allMessages, setAllMessages] = useState([]);
+    const [currentEcho, setCurrentEcho] = useState(undefined);
+    const { id } = useParams();
 
-  const scrollToBottom = () => {
-    scroll.current.scrollIntoView({ behavior: "smooth" });
-  };
+    const scrollToBottom = () => {
+        scroll.current.scrollIntoView({ behavior: "smooth" });
+    };
 
-  const getMessages = async () => {
-    props.fetchMessages(1, { chat_id: id });
-    setTimeout(scrollToBottom, 0);
-  };
+    const getMessages = async () => {
+        props.fetchMessages(1, { chat_id: id });
+        setTimeout(scrollToBottom, 0);
+    };
 
-  useEffect(() => {
-    getMessages();
-  }, []);
+    useEffect(() => {
+        getMessages();
+    }, []);
 
-  useEffect(() => {
-    if (currentEcho) {
-      currentEcho
-        .private(webSocketChannel)
-        .listen("MessageCreated", async (e) => {
-          console.log(e.message);
-          setAllMessages([...allMessages, e.message]);
-          await getMessages();
-        });
+    useEffect(() => {
+        if (currentEcho) {
+            currentEcho
+                .private(webSocketChannel)
+                .listen("MessageCreated", async (e) => {
+                    console.log(e.message);
+                    setAllMessages([...allMessages, e.message]);
+                    await getMessages();
+                });
 
-      return () => {
-        currentEcho.leave(webSocketChannel);
-      };
-    }
-  }, [currentEcho]);
+            return () => {
+                currentEcho.leave(webSocketChannel);
+            };
+        }
+    }, [currentEcho]);
 
-  useEffect(() => {
-    if (props.currentChat.id) {
-      console.log(props.currentChat);
-      var cEcho = new Echo({
-        broadcaster: "pusher",
-        key: import.meta.env.VITE_PUSHER_APP_KEY,
-        cluster: "eu",
-        encrypted: true,
-        authEndpoint: import.meta.env.VITE_API_URL + "/broadcasting/auth", // As I'm using JWT tokens, I need to manually set up the headers.
+    useEffect(() => {
+        if (props.currentChat.id) {
+            console.log(props.currentChat);
+            var cEcho = new Echo({
+                broadcaster: "pusher",
+                key: import.meta.env.VITE_PUSHER_APP_KEY,
+                cluster: "eu",
+                encrypted: true,
+                authEndpoint: import.meta.env.VITE_API_URL + "/broadcasting/auth", // As I'm using JWT tokens, I need to manually set up the headers.
+                auth: {
+                    headers: {
+                        Authorization: `Bearer ` + localStorage.token,
+                        Accept: 'application/json',
+                    },
+                },
+            });
 
-        // authEndpoint: import.meta.env.VITE_API_URL + "/channels/chats." + props.currentChat.socket,   // As I'm using JWT tokens, I need to manually set up the headers.
-        auth: {
-            headers: {
-                Authorization: `Bearer ` + localStorage.token,
-                // Accept: 'application/json',
-            },
-        },
-      });
+            setCurrentEcho(cEcho);
+        }
+    }, [props.currentChat]);
 
-      setCurrentEcho(cEcho);
-    }
-  }, [props.currentChat]);
+    useEffect(() => {
+        props.fetchChat(id);
+    }, []);
 
-  useEffect(() => {
-    props.fetchChat(id);
-  }, []);
-
-  return (
-    <Container>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <div className="card">
-            <div className="card-header">Chat Box</div>
-            <div
-              className="card-body"
-              style={{ height: "500px", overflowY: "auto" }}
-            >
-              {[...messages, ...allMessages]?.map((message) => (
-                <Message key={message.id} userId={1} message={message} />
-              ))}
-              <span ref={scroll}></span>
+    return (
+        <Container>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Chat Box</div>
+                        <div
+                            className="card-body"
+                            style={{ height: "500px", overflowY: "auto" }}
+                        >
+                            {[...messages, ...allMessages]?.map((message) => (
+                                <Message key={message.id} userId={1} message={message} />
+                            ))}
+                            <span ref={scroll}></span>
+                        </div>
+                        <div className="card-footer">
+                            <MessageInput
+                                user={props.currentUser}
+                                chat_id={id}
+                                createMessage={props.createMessage}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="card-footer">
-              <MessageInput
-                user={props.currentUser}
-                chat_id={id}
-                createMessage={props.createMessage}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </Container>
-  );
+        </Container>
+    );
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchMessages: (page, filters) => dispatch(fetchMessages(page, filters)),
-    fetchChat: (id) => dispatch(fetchChat(id)),
-    createMessage: (data) => dispatch(createMessage(data)),
-  };
+    return {
+        fetchMessages: (page, filters) => dispatch(fetchMessages(page, filters)),
+        fetchChat: (id) => dispatch(fetchChat(id)),
+        createMessage: (data) => dispatch(createMessage(data)),
+    };
 };
 
 const mapStateToProps = (state) => {
-  return {
-    currentChat: state.chat.current,
-    messages: state.message.data,
-    loading: state.message.loading,
-    currentUser: state.auth.user,
-  };
+    return {
+        currentChat: state.chat.current,
+        messages: state.message.data,
+        loading: state.message.loading,
+        currentUser: state.auth.user,
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
